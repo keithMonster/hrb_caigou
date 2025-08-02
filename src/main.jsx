d35c3cb98276db70857213764f6724bad045adfc
keithMonster <dongchaogekd@gmail.com>
2025-08-03 06:32:48 +0800

    refactor: 移除项目基础路径配置和部署工作流
    删除vite配置、路由和HTML中的基础路径/hrb_caigou
    移除不再需要的GitHub Pages部署工作流文件

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ConfigProvider locale={zhCN}>
-      <BrowserRouter basename="/hrb_caigou">
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </ConfigProvider>
